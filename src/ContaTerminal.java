8816839a3efb4e53f5aed52b0900e1f650a2ac89
ritdzn <ritdzn607@gmail.com>
2024-06-24 19:18:43 -0300

    criando mensagens

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+
+        //Saudações de entrada
+        System.out.println("Bem vindo ao aplicativo meu banco!  ");
+        System.out.println("Para seguir com seu acesso, precisaremos de algumas informações");
+
+
+        //Coleta de dados para Login
+        System.out.println("Favor informar o seu número de agência abaixo: ");
+        int numeroAgencia = scanner.nextInt();
+
+        System.out.println("Favor sua agência abaixo: ");
+        String agencia = scanner.next();
+
+        System.out.println("Favor informar seu nome abaixo: ");
+        String nomeCliente = scanner.next();
+
+
+
+        
+
+
+
+
+        
+
+
+        
+
     }
 }
