e41f21bcacffe5ca1c6589c1df34b8435698aec1
ritdzn <ritdzn607@gmail.com>
2024-06-24 19:38:37 -0300

    Projeto em andamento

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -5,7 +5,7 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-
+        
 
         //Saudações de entrada
         System.out.println("Bem vindo ao aplicativo meu banco!  ");
@@ -14,13 +14,23 @@ public class ContaTerminal {
 
         //Coleta de dados para Login
         System.out.println("Favor informar o seu número de agência abaixo: ");
-        int numeroAgencia = scanner.nextInt();
+        String numeroAgencia = scanner.next();
 
-        System.out.println("Favor sua agência abaixo: ");
-        String agencia = scanner.next();
+        System.out.println("Favor seu número de conta: ");
+        String numeroConta = scanner.next();
 
         System.out.println("Favor informar seu nome abaixo: ");
         String nomeCliente = scanner.next();
+
+        System.out.println("Favor informar seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+
+        //Concatenação das informações inseridas
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", sua conta é " + numeroConta + " e seu saldo " + saldo + ". Já está disponível para saque!" );
+        
+        
+
 
         
 
